2f3fb91f6dd80658576b139e835768ebafd16fa9
Guilherme Lenny <guilherme_kenay@outlook.com>
2024-06-03 20:52:24 -0300

    first commit

---
+++ desafio01-calculadora/src/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vgh;
+    background-color: #CACACA;
+
+    display: flex;
+    align-itens: center;
+    justify-content:center;
+`
+
+
+export const Content = styled.div`
+    background-color: #FAFAFA;
+    width: 80%;
+    min-height: 350px;
+`
